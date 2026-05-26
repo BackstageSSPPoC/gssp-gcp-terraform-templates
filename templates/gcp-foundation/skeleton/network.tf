ed5d127a40655ae3462054388d6f97e1f0c8e1c8
Suresh <sureshmavp1@gmail.com>
2026-05-26 13:41:48 +0530

    network.tf updated

--- templates/gcp-foundation/skeleton/network.tf
+++ templates/gcp-foundation/skeleton/network.tf
@@ -1,11 +1,44 @@
+########################################
+# EXISTING VPC
+########################################
+
+data "google_compute_network" "existing_vpc" {
+  count = var.use_existing_vpc ? 1 : 0
+
+  name = var.existing_vpc_name
+}
+
+########################################
+# EXISTING SUBNET
+########################################
+
+data "google_compute_subnetwork" "existing_subnet" {
+  count = var.use_existing_vpc ? 1 : 0
+
+  name   = var.existing_subnet_name
+  region = var.region
+}
+
+########################################
+# CREATE NEW VPC
+########################################
+
 resource "google_compute_network" "vpc" {
-  name                    = var.vpc_name
+  count = var.use_existing_vpc ? 0 : 1
+
+  name                    = "${var.environment}-${var.vpc_name}"
   auto_create_subnetworks = false
 }
 
+########################################
+# CREATE NEW SUBNET
+########################################
+
 resource "google_compute_subnetwork" "subnet" {
-  name          = var.subnet_name
+  count = var.use_existing_vpc ? 0 : 1
+
+  name          = "${var.environment}-${var.subnet_name}"
   ip_cidr_range = var.subnet_cidr
   region        = var.region
-  network       = google_compute_network.vpc.id
+  network       = google_compute_network.vpc[0].id
 }
