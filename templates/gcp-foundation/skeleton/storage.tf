6963d90b12f32f66018196d685640fd61a908a5d
Chaitanya Shekhar Pandey <raichaitanya2920@gmail.com>
2026-05-27 14:19:53 +0000

    Adding terraform Destroy

--- templates/gcp-foundation/skeleton/storage.tf
+++ templates/gcp-foundation/skeleton/storage.tf
@@ -2,6 +2,6 @@ resource "google_storage_bucket" "bucket" {
   count                       = var.create_bucket ? 1 : 0
   name                        = lower("${var.environment}-${var.app_name}-${var.bucket_name}")
   location                    = var.region
-  force_destroy               = true
+  force_destroy               = false
   uniform_bucket_level_access = true
 }
