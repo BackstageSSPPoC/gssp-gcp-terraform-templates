bffccba708663d1e93d0c7fdba929bb9e82f8b19
Chaitanya Shekhar Pandey <113713770+Chaitanya-Shekhar-Pandey@users.noreply.github.com>
2026-05-27 13:24:53 +0530

    Update storage.tf

--- templates/gcp-foundation/skeleton/storage.tf
+++ templates/gcp-foundation/skeleton/storage.tf
@@ -1,5 +1,5 @@
 resource "google_storage_bucket" "bucket" {
-  name                        = lower("${var.environment}-${var.bucket_name}")
+  name                        = lower("${var.environment}-${var.app_name}-${var.bucket_name}")
   location                    = var.region
   force_destroy               = true
   uniform_bucket_level_access = true
